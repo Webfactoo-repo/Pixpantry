0e9bd862444248e0e71ab2099a39c52c06c4729a
lordcase <lordcase@gmail.com>
2014-09-02 01:24:23 +0200

    Edit - randomize search (not working)

--- wp-content/themes/stock-photography/functions.php
+++ wp-content/themes/stock-photography/functions.php
@@ -309,3 +309,14 @@ function new_mail_from($old) {
 function new_mail_from_name($old) {
  return 'Pixpantry';
 }
+/*
+function search_filter($query) {
+  if ( !is_admin() && $query->is_main_query() ) {
+    if ($query->is_search) {
+      $query->set('orderby', 'rand');
+    }
+  }
+}
+
+add_action('pre_get_posts','search_filter');
+/*
